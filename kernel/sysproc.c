d854aa8eefd9dbbc8d9a1c1dfe8e5da144ba765f
zivzalzm <zivzalzm@gmail.com>
2026-04-25 15:47:44 +0000

    Implement sys_co_yield in sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -89,3 +89,64 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_co_yield(void)
+{
+  int target_pid, value;
+  argint (0, &target_pid);
+  argint (1, &value);
+
+  struct proc *p = myproc();
+
+  // error: invalid pid or self-yield
+  if (target_pid <= 0 || target_pid == p->pid){
+    return -1;
+  }
+
+  // find target process in the proc table
+  extern struct proc proc[];
+  struct  proc *target = 0;
+  for (struct proc *t = proc; t < &proc[NPROC]; t++){
+    acquire(&t->lock);
+    if (t->pid == target_pid){
+      // errors
+      if (t->state == UNUSED || t->state == ZOMBIE || t->killed){
+        release (&t->lock);
+        return -1;
+      }
+
+      target = t;
+      break;
+    }
+    release(&t->lock);
+  }
+  
+  if (target == 0){
+    return -1;
+  }
+  //target->lock is held here
+
+  //if target is already waiting in co_yield (chan == target is our sentinel)
+  if (target->state == SLEEPING && target->chan == (void*)target) {
+    target->trapframe->a0 = (uint64)value; // give target our value
+    target->state = RUNNABLE;
+  }
+  release(&target->lock);
+
+  //sleep ourselves, waiting for target to co_yield back to us
+  acquire(&p->lock);
+  p->chan = (void*)p; // sentinel: I`m sleeping in co_yield
+  p->state = SLEEPING;
+  sched();            // give up the CPU
+  p->chan = 0;
+  release(&p->lock);
+
+  // if we were killed while sleeping, return error
+  if (killed(p)){
+    return -1;
+  }
+
+  return (int)p->trapframe->a0; // value set by whoever woke us
+
+}
